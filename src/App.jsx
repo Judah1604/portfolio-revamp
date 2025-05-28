fdf2ae728a5fc6692a03daad707b95d3fe92c34d
Judah Oyedele <81442563+Judah1604@users.noreply.github.com>
2025-05-28 22:07:05 +0100

    added files and scaffolded project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/base/common.css";
+import Nav from "./components/Nav/Nav";
+
+function App() {
+	return <Nav />;
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,26 @@
+import "./nav.css";
+
+function Nav() {
+	return (
+		<>
+			<img src="/Effects/stars.png" className="stars-img" />
+			<nav className="navbar">
+				<div className="container">
+					<a href="/" className="navbar-brand">
+						<img src="/wordmark.png" alt="Aurora" />
+					</a>
+					<div className="navlinks">
+						<a href="#services">Services</a>
+						<a href="#projects">Projects</a>
+					</div>
+					<button className="btn btn-blue icon-left">
+						<img src="/Icons/send.svg" alt="send" />
+						Work With Me
+					</button>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default Nav;
