eaf9c908983d7a772e274778e478b3a5e6fbabca
Judah Oyedele <81442563+Judah1604@users.noreply.github.com>
2025-05-31 16:53:04 +0100

    finished about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,4 @@
+import Services from "../Services/Services";
 import "./about.css";
 
 function About() {
@@ -15,7 +16,10 @@ function About() {
 						and beautiful web interfaces. With a strong eye for
 						design and a frontend toolkit to match, I bridge
 						creativity and code to deliver experiences that{" "}
-						<a target="_blank" href="https://wa.me/08166085206">click</a>.
+						<a target="_blank" href="https://wa.me/08166085206">
+							click
+						</a>
+						.
 					</h1>
 					<img
 						className="highlight"
@@ -26,6 +30,49 @@ function About() {
 				<img className="glow1" src="/Effects/Star_1.png" alt="Glow" />
 				<img className="glow2" src="/Effects/Star_2.png" alt="Glow" />
 			</div>
+			<Services />
+            <div className="info-wrapper">
+
+			<div className="info container text-center">
+				<img
+					src="/Effects/Star.png"
+					alt="Star"
+					className="section-highlight"
+				/>
+				<h1 className="header">About Me!</h1>
+				<div className="row mt-4">
+					<div className="text col-md-7">
+						Hey, I’m Judah Oyedele — a{" "}
+						<b>frontend developer and UI/UX designer</b> with over{" "}
+						<b>5 years of experience</b> crafting web experiences
+						that not only <b>look good,</b> but <b>convert</b>. I
+						prioritize conversion in my designs because beauty and
+						results are both crucial to a good design. Whether it’s
+						tweaking user flows, optimizing layouts, or building
+						smooth interactions, I focus on making every click
+						count.
+						<p>
+							Combining clean code with smart design, I create
+							interfaces that guide users effortlessly and drive
+							real impact. Let’s build something that not only
+							turns heads but turns visitors into loyal users.
+						</p>
+						<button className="btn btn-blue icon-left">
+							<img src="/Icons/download.svg" />
+							Download Resume
+						</button>
+					</div>
+					<img
+						src="/Images/Profile.png"
+						alt="Image"
+						className="col-md-4"
+					/>
+                    <img src="/Effects/doodle.png" alt="doodle" className="doodle" />
+				</div>
+			</div>
+                <img className="glow1" src="/Effects/Star_1.png" alt="Glow" />
+                <img className="glow2" src="/Effects/Star_2.png" alt="Glow" />
+            </div>
 		</>
 	);
 }
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,46 @@
+import "./services.css";
+
+function Services() {
+	return (
+		<div className="services container text-center">
+			<img
+				src="/Effects/Star.png"
+				alt="Star"
+				className="section-highlight"
+			/>
+			<h1 className="header">Services</h1>
+			<div className="container row">
+				<div className="service design col-md-5">
+					<img src="/Icons/pen-tool.svg" alt="design" />
+
+					<h1>UI/UX Design</h1>
+					<p>
+						I design clean, intuitive interfaces that guide users
+						effortlessly and drive action. With tools like Figma and
+						a sharp eye for detail, I focus on layout, hierarchy,
+						and consistency to create designs that don’t just look
+						good — they work. Whether it’s a landing page or a full
+						web app, I always design with purpose and conversion in
+						mind.
+					</p>
+				</div>
+				<div className="service dev col-md-5">
+					<img src="/Icons/code.svg" alt="code" />
+
+					<h1>Frontend Web Development</h1>
+					<p>
+						I turn designs into responsive, interactive web
+						experiences using HTML, CSS, JavaScript, and React. I
+						care about clean code, performance, and making sure
+						everything works smoothly across devices. Whether I’m
+						building a full site or reusable components, I focus on
+						creating seamless, user-friendly interfaces that bring
+						designs to life — and keep users engaged.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Services;
